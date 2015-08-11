393d09a6f02d916d4bce31111245eb9c5dcbb14e
Paulo Henrique <paulohenrique@eggtech.com.br>
2015-08-11 16:11:23 -0300

    Aula 11 - Criando Diretivas - Parte 2 - Criação da diretiva uiCpf que aplica a mascara no formato 999.999.999-99

---
+++ aulas_rodrigo_branas/Aula_11_Criando_Diretivas_Parte_2/js/directives/uiCpfDirective.js
@@ -0,0 +1,32 @@
+/// <reference path="../../../../typings/angularjs/angular.d.ts"/>
+angular.module('ListaTelefonica').directive('uiCpf', function(){
+	return {
+		require: 'ngModel',
+		link: function(scope, element, attrs, ctrl){
+			
+			//XXX.XXXX.XXX-XX
+			var _formatCpf = function(cpf){
+			
+				if(!cpf) return cpf;
+				cpf = cpf.replace(/[^0-9]+/g,'');
+
+				if(cpf.length > 3)
+					cpf = cpf.substring(0,3) + '.' + cpf.substring(3);
+				
+				if(cpf.length > 7)
+					cpf = cpf.substring(0,7) + '.' + cpf.substring(7);;
+				 
+				if(cpf.length > 11)
+					cpf = cpf.substring(0,11) + '-' + cpf.substring(11,13);
+					
+				console.log(cpf.substring(0,3));
+				return cpf;
+			};
+			
+			element.bind('keyup', function(){
+				ctrl.$setViewValue(_formatCpf(ctrl.$viewValue));
+				ctrl.$render();
+			});
+		}
+	}
+});
