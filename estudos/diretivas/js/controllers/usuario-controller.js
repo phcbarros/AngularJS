149ba9ae637045495276bc45eacb465979e1e5cb
Paulo Henrique da Costa Barros <paulo_costinha@hotmail.com>
2015-09-12 14:54:18 -0300

    Estudos - Diretivas - Criando Diretiva com campoes editaveis

---
+++ estudos/diretivas/js/controllers/usuario-controller.js
@@ -0,0 +1,38 @@
+'use strict';
+angular.module('myApp')
+	.controller('UsuarioController', ['$scope', usuarioController]);
+
+function usuarioController($scope){
+	
+	var init = function(){
+		$scope.usuarios = [
+			{id: 1, nome: 'Maria da Silva', idade: 27, email:'mariasilva@contato.com.br', dataNascimento: '12/01/1987'},
+			{id: 2, nome: 'João de Marco', idade: 35, email:'jdemarco@contato.com.br', dataNascimento: '27/02/1980'},
+			{id: 3, nome: 'Evelin Mello', idade: 18, email:'evellin@contato.com.br', dataNascimento: '30/08/1997'},
+			{id: 4, nome: 'Thor de Asgard', idade: 30, email:'thor@contato.com.br', dataNascimento: '10/05/1985'},
+			{id: 5, nome: 'Tony S. Tark', idade: 27, email:'tonystark@contato.com.br', dataNascimento: '01/07/1987'},
+		];	
+	};
+	
+	$scope.update = function(user){
+		var index =  $scope.usuarios.indexOfById(user.id);
+		$scope.usuarios.splice(index,1,user); //substituindo o elemento no array
+		console.log('atualizou', $scope.usuarios[index]);
+	};
+	
+	init();
+};
+
+Array.prototype.indeOfById = function(id){
+	var index = -1,
+        i = 0,
+        l = this.length;
+		
+    for (; l--; i++) {
+        var item = this[i];
+        if (item.id === parseInt(id)) {
+            return index = i;
+        }
+    }
+    return index;
+};
---
+++ estudos/diretivas/js/directives/ui-table-editable.js
@@ -0,0 +1,72 @@
+angular.module('myApp')
+	.directive('containerEditable', [createContainerEditable])
+	.directive('editableInput', [createEditableInput]);
+	
+function createContainerEditable(){
+	return {
+		restrict: 'AE',
+		controller: function($scope, $element, $attrs){
+			var inputs = [];
+			
+			this.registerInput = function(input){
+				inputs.push(input);
+			};
+			
+			this.destroyAllEditableInput = function(){
+				inputs.forEach(function(input){
+					delete input.editable;
+				});
+			};
+			
+			this.destroyEditableInput = function(input){
+				delete input.editable;
+			};
+			
+		}
+	};
+};
+
+function createEditableInput(){
+	return {
+		require: '^containerEditable',
+		transclude: true,
+		templateUrl: 'view/ui-table-editable.html',
+		scope:{
+			field: '@',
+			user: '=',
+			action: '&'
+		},
+		link: function(scope, element, attrs, ctrl){
+			
+			ctrl.registerInput(scope);
+			
+			scope.show = function(){
+				return scope.editable === scope.field;	
+			};
+			
+			scope.cancel = function(e, copy, user){
+				e.stopPropagation();
+				ctrl.destroyEditableInput(this);					
+			};
+			
+			scope.update = function(e, copy){
+				e.stopPropagation();
+				ctrl.destroyEditableInput(this);
+				scope.user = copy	
+				scope.action({user: scope.user});		
+			};
+			
+			element.on('click', function(e){
+				e.stopPropagation();
+				ctrl.destroyAllEditableInput();
+				
+				scope.$apply(function(){
+					scope.copy = angular.copy(scope.user);
+					scope.editable = scope.field;
+				});
+				
+			});
+			
+		}
+	};
+};
