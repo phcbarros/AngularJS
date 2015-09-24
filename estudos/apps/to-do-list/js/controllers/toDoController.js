1748d20fa2b4e8727738b3384482954b51cfc7ab
Paulo Henrique da Costa Barros <paulo_costinha@hotmail.com>
2015-09-23 21:25:32 -0300

    Estudos - Apps - ToDoList - Retirada da diretiva ng-change

--- estudos/apps/to-do-list/js/controllers/toDoController.js
+++ estudos/apps/to-do-list/js/controllers/toDoController.js
@@ -2,11 +2,7 @@ angular.module('myApp')
 	.controller('toDoController', ['$scope', toDoController]);
 	
 function toDoController($scope){
-	
-	$scope.change = function(tarefa){
-		tarefa.isSelected = tarefa.isSelected;
-	};
-	
+		
 	$scope.add = function(tarefa){
 		$scope.tarefas.push(angular.copy(tarefa));
 		delete tarefa.nome;	
