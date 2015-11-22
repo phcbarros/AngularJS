2e18885c644061a516291822bf15130e2aff5741
Paulo Henrique da Costa Barros <paulo_costinha@hotmail.com>
2015-11-22 14:24:45 -0200

    Estudos - EventModel - Usando service para efetuar a cominicação entre controllers

--- estudos/apps/eventModel/js/controllers/eventModelCtrl.js
+++ estudos/apps/eventModel/js/controllers/eventModelCtrl.js
@@ -2,29 +2,59 @@ angular.module("eventModel")
 	.controller("fruitCtrl", fruitCtrl)
 	.controller("deliveryCtrl", deliveryCtrl)
 	.factory("logService", logService)
+	.factory("notificationService", notificationService)
 	.directive("fruit", fruit)
 	.directive("fruitTracker", fruitTracker);
 
-function fruitCtrl($scope, $rootScope){
+function fruitCtrl($scope, $rootScope, notificationService){
 	$scope.fruits = ["Banana", "Uva", "Jabuticaba", "Acerola"];
-		
-	$scope.deliver = function(fruit){
-		$rootScope.$broadcast("delivery.request", fruit);
+	$scope.events = [];
+	
+	function clear(){
 		delete $scope.fruit;
 	};
+	
+	$scope.deliverBroadcast = function(fruit){
+		$rootScope.$broadcast("delivery.request", fruit);
+		clear();
+	};
+	
+	$scope.deleveryNotificationService = function(fruit){
+		notificationService.notify(fruit);
+		clear();
+	};
+	
+	//notificationService.load();
+	// notificationService.subscribe(function(data){
+	// 	$scope.events.push(data);
+	// 	$scope.$apply();
+	// });
 		
 };
 
-function deliveryCtrl($scope, $timeout, logService){
+function deliveryCtrl($scope, $timeout, logService, notificationService){
+	
+	//using $broadcast
 	$scope.$on("delivery.request", function(e, data){
+		exibirDados(data);
+	});
+	
+	//using notificationService
+	notificationService.subscribe(function(data){
+		exibirDados(data);
+	});
+	
+	var exibirDados = function(data){
 		$scope.received = true;
 		$scope.delivery = data;
+		
 		logService.track("delivery.request", data);
+		
 		$timeout(function(){
 			$scope.received = false;
 			delete $scope.delivery;
 		}, 1000);
-	});
+	}
 };
 	
 function fruit(){
@@ -62,3 +92,27 @@ function logService($log){
 	};
 };
 
+
+function notificationService($rootScope){
+	function notify(data){
+		$rootScope.$emit("notificationService.update", data);	
+	};
+	
+	function listen(fn){
+		$rootScope.$on("notificationService.update", function(e, data){
+			fn(data);	
+		});
+	};
+	
+	// Anything that might have a reason
+   	// to emit events at later points in time
+   	function load () {
+    	setInterval(notify.bind(null, 'Something happened!'), 1000);
+   	};
+	
+	return{
+		subscribe: listen,
+		notify: notify,
+		load: load
+	};
+};
