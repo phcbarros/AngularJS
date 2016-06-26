21ac190410b8277b09c3891d14dec0f9e49b66ad
Paulo Henrique da Costa Barros <paulo_costinha@hotmail.com>
2016-06-25 22:14:44 -0300

    Angular 1.5 - Criado as tasks dist-css e clean-dist-css

--- Angular1.5/LifeCycle_Hooks/gulpfile.js
+++ Angular1.5/LifeCycle_Hooks/gulpfile.js
@@ -9,11 +9,15 @@ var del = require('del');
 var fs = require('fs');
 var babelify = require('babelify');
 var browserify = require('browserify');
+var less = require('gulp-less');
+var LessAutoprefix = require('less-plugin-autoprefix');
+var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 
 var paths = {
     rootPath: 'dist/',
     src: {
         js: ['src/**/*.js'],
+        css: ['src/**/*.less']
     },
     vendor: {
         js: ['node_modules/angular/angular.min.js']
@@ -30,17 +34,17 @@ gulp.task('dist-js', ['browserify'], function () {
         .pipe(gulp.dest(paths.rootPath + 'app/js/'));
 });
 
-// gulp.task('dist-js', ['clean-js'], function () {
-//     return gulp.src(paths.src.js)
-//         .pipe(sourcemaps.init())
-//         .pipe(babel({
-//             presets: ['es2015'],
-//             plugins: ['transform-runtime', 'transform-es2015-classes']
-//         }))
-//         .pipe(concat('app.bundle.js'))
-//         .pipe(sourcemaps.write())
-//         .pipe(gulp.dest(paths.rootPath + 'app/js/'));
-// });
+gulp.task('browserify', function () {
+   return browserify('src/app.js', { debug: true })
+        .transform(babelify)
+        .bundle()
+        .on("error", function (err) { console.log("Error: " + err.message); })
+        .pipe(fs.createWriteStream('app.bundle.js'));
+});
+
+gulp.task('del', function(){
+    return del('app.bundle.js');
+});
 
 gulp.task('clean-js-vendor', function () {
     return gulp.src(paths.rootPath + 'vendor/js/')
@@ -60,21 +64,24 @@ gulp.task('dist-js-vendor', function () {
         .pipe(gulp.dest(paths.rootPath + '/vendor/js'));
 });
 
-gulp.task('dist-app', ['clean-js', 'dist-js-vendor', 'dist-js']);
+gulp.task('clean-dist-css', function() {
+    return gulp.src(paths.rootPath + 'app/css/')
+        .pipe(clean());
+});
+
+gulp.task('dist-css', ['clean-dist-css'], function(){
+    return gulp.src(paths.src.css)
+        .pipe(sourcemaps.init())
+        .pipe(less({
+            plugins: [autoprefix]
+        }))
+        .pipe(concat('styles.bundle.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.rootPath + 'app/css/'));
+});
+
+gulp.task('dist-app', ['clean-js', 'dist-js-vendor', 'dist-js', 'dist-css']);
 
 gulp.task('default', function (done) {
     runSequence('browserify',['dist-app'], 'del', done);
-});
-
-gulp.task('browserify', function () {
-
-   return browserify('src/app.js', { debug: true })
-        .transform(babelify)
-        .bundle()
-        .on("error", function (err) { console.log("Error: " + err.message); })
-        .pipe(fs.createWriteStream('app.bundle.js'));
-});
-
-gulp.task('del', function(){
-    return del('app.bundle.js');
 });
